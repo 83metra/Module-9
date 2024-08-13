c50ce804b3b1dd10bc5fb758796c18df165fb2c6
83metra <jumo_004@mail.ru>
2024-08-13 16:57:39 +0200

    Update Module 9 1.py
    Добавлено исключение, если в списке чисел присутствует не число.

--- Module 9 1.py
+++ Module 9 1.py
@@ -1,8 +1,11 @@
 def  apply_all_func(int_list, *functions):
     results = {}
-    for function in functions:
-        results.update({function.__name__: function(int_list)})
-    return results
+    try:
+        for function in functions:
+            results.update({function.__name__: function(int_list)})
+        return results
+    except TypeError:
+        return f'Переданы неверные данные, должно быть только число!'
 
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
