7b7180154fb933fe1099b0ff0d67bb79b5d2a7b2
83metra <jumo_004@mail.ru>
2024-08-16 11:14:40 +0200

    Update Module 9 6.py
    Убран лишний цикл while len_ != 0, так как он не нужен.

--- Module 9 6.py
+++ Module 9 6.py
@@ -1,10 +1,9 @@
 def all_variants(text):
     len_ = len(text)
-    while len_ != 0:
-        for y in range(len_): # сколько букв добавляем в комбинацию.
-            for i in range(len_): # позиция перебора
-                yield text[i:i+y+1]
-            len_ = len_ - 1
+    for y in range(len_): # сколько букв добавляем в комбинацию.
+        for i in range(len_): # позиция перебора
+            yield text[i:i+y+1]
+        len_ = len_ - 1
 
 a = all_variants("abc")
 for i in a:
